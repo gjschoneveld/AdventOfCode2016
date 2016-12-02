a49c4c0f03662f4eeaf4ddc2d2edaafd0c8775a2
gjschoneveld <gj_schoneveld@ymail.com>
2016-12-02 21:48:07 +0100

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = { "LUULRUULULLUDUDULDLUDDDLRURUDLRRDRDULRDDULLLRULLLURDDLRDLUUDDRURDDRDDDDRDULULLLLURDDLLRLUUDDDRLRRRDURLDDLRRLDUDRRRDLDLRRDLDLUURRLRULLULRUDRDLRUURLDRDLRLDULLLUDRDDRLURLUUDRLLLDRUUULLUULRUDDUDRDUURRRUDRLDDUURDUURUDRDDLULDDUDUDRRDDULUDULRDRULRLRLURURDULRUULLRDDDDRRUUDDDUUDRLLRUDRLRDLRRLULRLULRUDDULRLLLURLDDRLDDLRRLDRDDDRRLRUDRULUUDUURLDLRRULUDRDULDLLRRURRDDLRRRLULUDUUDDUDDLRDLRDRLRLDUDUDDUDLURRUURDRLRURLURRRLRLRRUDDUDDLUDRLUURUUDUUDDULRRLUUUDRLRLLUR",
+                            "LDLLRRLDULDDRDDLULRRRDDUDUDRRLLRUUULRUDLLRRDDRRLDDURUUDLUDRRLDURDDRUDLUDUUDLDLLLDLLLDRLLDLRUULULLUUDULDUUULDDLRUDLLUDLUUULDRLUDRULUUDLDURDLDUULLRDUDRDLURULDLUUUDURLDDRLLDRLRDDDUDRUULLDLUDRRDDLDLUURUDDLDRURRLULUDDURLDRDRDUDDRRULRLDURULULRURDUURRUDRDDRDRLDRDUUDLRULRDDDULRURUDRUUULUUDDLRRDDDUDRLRUDRDLRRUDLUDRULDDUDLRLDDLDRLRDLULRDRULRLLRLUDUURULLLDDUULUUDDDUDRRULDDDULRUDRRLRLLLUDLULDUUULDDULDUUDLUULRDLDUDRUDLLDLDLLULDDDDLUDDUDRUDLRRRDDDDDLLRRDRUUDDDRRULRUDUUDRULLDLLLDDRDDUURLUUURUDRUDURLRUUUULUUURDRRRULDUULDLDDDRDDDDLLDRUDRDURLDDURDURULDDRLLRRLDUDRDURRLDRDLLULUUUD",
+                            "LDDLRLRDDRLRUDDRDDUDRULUUULULDULRUULLRRDUULRDUUDDDRRULDDUDRLLLDULURDLDDRLLRURULULDLDULRDLDLRULUDLLDRUDLDURRDULDDRLRURDLLUDRDDDUDLUDULURULRDRLRULDLLRLDRRUDRDRUDRLDLRLUUURURRRLDDULLULLLRLRLULDLLRLDDRLDULURULRUURRUUURRUDRLRRURURDDDRULDULDLDLRRRLLDDRRURRULULULDRDULDRRULDUDRRLDULDRDURRDULLRRRLLLLRRLLRRRDRURDUULLURURURDDRRDRLLLULRRRDRLDRLDRDLLRUUDURRDRRDLLUDLDRLRLDLUDRDULRULRRLLRDLULDRLUDUUULLDRULDDLLRDUUUDRUUUUULUURDDLLDUURURRURLLURRDDUDUDRUUDDRDDRRLRLULRLRRRDRLLRRLLLDUULLUUDDLULLLDURRLLDRLDRDRLRRLRRULRRRRLRRRRRURUDULUULRDLLDRLRRDUURDRRUDRURRRDDRLDDLRLUDRDRDRRLDDDRDDRRRDUDULRURRDRDLLDRUD",
+                            "UUUDLDDLRDLLLLRUUURDDLLURRUUURLUULLURUUDUDLDULULLRRRRLLLRDLLUDRUURDRURUDRURRLRLDRURLUDRLULRRURDDDURLLDULDLRRRDUUDDDRDLRUURRDRDRLRDLULRLDDRULRULDRDUDRUURLDLUDDULLLRURRLURLULDRRLUUURURLDLDDULLLRUUURDDDUURULULLUUUDUDRLLRRULUULDDDLLUDLURLLLRRULLURDRLUUDDLLDLLLUDULLRDRRRURDRUDUDUULUDURDLRUDLLRDDRURUDURLRULURDDURULLRDDRLRRDRLLULRDDDULRDLRULDDLRRDULDLUURRURUULRRDUURUDRRRRRLDULDLRURRULULDLRDDDRLLDURRULDUDUDRRRLUULRLUDURRRLRLDURRRRUULDRLUDDDUDURLURUDLLUDRDDDRLLURLRLDDURUUDDDUDUR",
+                            "RURRRRURUDDRLURUDULRDUDDDUURULDRRRRURDLDRRLLDLUDLRRLRRUULLURULLRDLLRDDDDULLRLLDDLLRUDDULDUDLDURLRUULDDURURDURDLDRRULRURRRRRLRRLLUDURRURULRLRDLRLRRRLLURURDLLLDLDDULDLUDDLLLRUDDRDRLRUDRRLDDLRDLRLRLRLRRDUUURRUDRRLDLRRUULULLUDRRRUDLURDRUULDRDRRLUULULDDLURRLDULLURLDRLDULDRLLDLUUULLULRRDDRURRURLDLDRRLLLLLUDUURUULURLRDDDLRRRRLLLURUDLDDRDDRRUDURUULDRRULLLRRLRULLLRLDDLLRRLRURLRDRUDULLDDLDDDDDLDURURDLULRDDLRDLLRURLLRDLRUDDRDRRDURDURLUDRLDUDDDRRURRLUULURULLRLRDLRRLRURULLDDURLLRRRUDDRDLULURRRUUUULUULRRLLDLRUUURLLURLUURRLRL" };
+
+            int current1 = 5;
+
+            string code1 = "";
+
+            foreach (var commands in input)
+            {
+                foreach (var cmd in commands)
+                {
+                    switch (cmd)
+                    {
+                        case 'U':
+                            if (current1 >= 4)
+                            {
+                                current1 -= 3;
+                            }
+                            break;
+                        case 'D':
+                            if (current1 <= 6)
+                            {
+                                current1 += 3;
+                            }
+                            break;
+                        case 'L':
+                            if (current1 % 3 != 1)
+                            {
+                                current1 -= 1;
+                            }
+                            break;
+                        case 'R':
+                            if (current1 % 3 != 0)
+                            {
+                                current1 += 1;
+                            }
+                            break;
+                    }
+                }
+
+                code1 += current1;
+            }
+
+            Console.WriteLine("Answer 1: {0}", code1);
+
+
+            var left = new Dictionary<char, char>
+            {
+                {'3', '2'},
+                {'4', '3'},
+                {'6', '5'},
+                {'7', '6'},
+                {'8', '7'},
+                {'9', '8'},
+                {'B', 'A'},
+                {'C', 'B'},
+            };
+
+            var up = new Dictionary<char, char>
+            {
+                {'3', '1'},
+                {'6', '2'},
+                {'7', '3'},
+                {'8', '4'},
+                {'A', '6'},
+                {'B', '7'},
+                {'C', '8'},
+                {'D', 'B'},
+            };
+
+            var right = new Dictionary<char, char>();
+            foreach (var kv in left)
+            {
+                right.Add(kv.Value, kv.Key);
+            }
+
+            var down = new Dictionary<char, char>();
+            foreach (var kv in up)
+            {
+                down.Add(kv.Value, kv.Key);
+            }
+
+            char current2 = '5';
+
+            string code2 = "";
+
+            foreach (var commands in input)
+            {
+                foreach (var cmd in commands)
+                {
+                    Dictionary<char, char> directions = null;
+                    switch (cmd)
+                    {
+                        case 'U':
+                            directions = up;
+                            break;
+                        case 'D':
+                            directions = down;
+                            break;
+                        case 'L':
+                            directions = left;
+                            break;
+                        case 'R':
+                            directions = right;
+                            break;
+                    }
+
+                    if (directions.ContainsKey(current2))
+                    {
+                        current2 = directions[current2];
+                    }
+                }
+
+                code2 += current2;
+            }
+
+            Console.WriteLine("Answer 2: {0}", code2);
+
+            Console.ReadKey();
+        }
+    }
+}
