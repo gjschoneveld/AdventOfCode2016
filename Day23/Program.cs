3c267c5156f9b9cff1a93d4e569beb8a1331afeb
gjschoneveld <gj_schoneveld@ymail.com>
2016-12-25 13:29:05 +0100

    Day23 & Day24

---
+++ Day23/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day23
+{
+    class Computer
+    {
+        public string[] instructionMemory;
+        private int programCounter;
+        private Dictionary<string, int> registerFile = new Dictionary<string, int>
+        {
+            { "a", 0 },
+            { "b", 0 },
+            { "c", 0 },
+            { "d", 0 }
+        };
+
+        public int Read(string x)
+        {
+            if (char.IsLetter(x, 0))
+            {
+                return registerFile[x];
+            }
+
+            return int.Parse(x);
+        }
+
+        public void Write(string reg, int val)
+        {
+            if (registerFile.ContainsKey(reg))
+            {
+                registerFile[reg] = val;
+            }
+        }
+
+        public void Run()
+        {
+            while (0 <= programCounter && programCounter < instructionMemory.Length)
+            {
+                var instruction = instructionMemory[programCounter];
+
+                var parts = instruction.Split(' ');
+                var opcode = parts[0];
+
+                int offset = 1;
+                switch (opcode)
+                {
+                    case "cpy":
+                        Write(parts[2], Read(parts[1]));
+                        break;
+                    case "inc":
+                        Write(parts[1], Read(parts[1]) + 1);
+                        break;
+                    case "dec":
+                        Write(parts[1], Read(parts[1]) - 1);
+                        break;
+                    case "jnz":
+                        if (Read(parts[1]) != 0)
+                        {
+                            offset = Read(parts[2]);
+                        }
+                        break;
+                    case "tgl":
+                        var location = programCounter + Read(parts[1]);
+                        if (location < 0 || instructionMemory.Length <= location)
+                        {
+                            break;
+                        }
+
+                        var target = instructionMemory[location];
+                        var targetParts = target.Split(' ');
+
+                        if (targetParts.Length == 2)
+                        {
+                            if (targetParts[0] == "inc")
+                            {
+                                targetParts[0] = "dec";
+                            }
+                            else
+                            {
+                                targetParts[0] = "inc";
+                            }
+                        }
+                        else if (targetParts.Length == 3)
+                        {
+                            if (targetParts[0] == "jnz")
+                            {
+                                targetParts[0] = "cpy";
+                            }
+                            else
+                            {
+                                targetParts[0] = "jnz";
+                            }
+                        }
+
+                        instructionMemory[location] = string.Join(" ", targetParts);
+                        break;
+                }
+
+                programCounter += offset;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] input = {
+                "cpy 2 a",
+                "tgl a",
+                "tgl a",
+                "tgl a",
+                "cpy 1 a",
+                "dec a",
+                "dec a"
+            };
+             */
+
+            string[] input = {
+                "cpy a b",
+                "dec b",
+                "cpy a d",
+                "cpy 0 a",
+                "cpy b c",
+                "inc a",
+                "dec c",
+                "jnz c -2",
+                "dec d",
+                "jnz d -5",
+                "dec b",
+                "cpy b c",
+                "cpy c d",
+                "dec d",
+                "inc c",
+                "jnz d -2",
+                "tgl c",
+                "cpy -16 c",
+                "jnz 1 c",
+                "cpy 96 c",
+                "jnz 91 d",
+                "inc a",
+                "inc d",
+                "jnz d -2",
+                "inc c",
+                "jnz c -5"
+            };
+
+            var comp1 = new Computer
+            {
+                instructionMemory = input.Clone() as string[]
+            };
+
+            comp1.Write("a", 7);
+
+            comp1.Run();
+
+            var a1 = comp1.Read("a");
+
+            Console.WriteLine("Answer 1: {0}", a1);
+
+
+            var comp2 = new Computer
+            {
+                instructionMemory = input.Clone() as string[]
+            };
+
+            comp2.Write("a", 12);
+
+            comp2.Run();
+
+            var a2 = comp2.Read("a");
+
+            Console.WriteLine("Answer 2: {0}", a2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Day24
+{
+    class Positon
+    {
+        public int x;
+        public int y;
+    }
+
+    class State : IEquatable<State>
+    {
+        public int x;
+        public int y;
+
+        public bool[] visited;
+
+        public char[,] maze;
+
+        public State[] Next()
+        {
+            List<State> result = new List<State>();
+
+            Positon[] neighbours = {
+                new Positon { x = x, y = y - 1 },
+                new Positon { x = x - 1, y = y },
+                new Positon { x = x + 1, y = y },
+                new Positon { x = x, y = y + 1 }
+            };
+
+            foreach (var nb in neighbours)
+            {
+                if (maze[nb.y, nb.x] == '#')
+                {
+                    continue;
+                }
+
+                var newVisited = visited;
+                if (maze[nb.y, nb.x] != '0' && char.IsDigit(maze[nb.y, nb.x]))
+                {
+                    newVisited = visited.Clone() as bool[];
+                    newVisited[maze[nb.y, nb.x] - '0' - 1] = true;
+                }
+
+                result.Add(new State
+                {
+                    x = nb.x,
+                    y = nb.y,
+                    visited = newVisited,
+                    maze = maze
+                });
+            }
+
+            return result.ToArray();
+        }
+
+        public override int GetHashCode()
+        {
+            var hash = (x << 16) ^ y;
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                hash <<= 1;
+                if (visited[i])
+                {
+                    hash |= 1;
+                }
+            }
+
+            return hash;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as State;
+            if (other == null)
+            {
+                return false;
+            }
+            
+            return Equals(other);
+        }
+
+        public bool Equals(State other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            bool equalX = x == other.x;
+            bool equalY = y == other.y;
+            bool equalVisited = visited.SequenceEqual(other.visited);
+
+            return equalX && equalY && equalVisited;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            var maze = new char[input.Length, input[0].Length];
+            int max = 0;
+            int startX = 0;
+            int startY = 0;
+
+            for (int r = 0; r < input.Length; r++)
+            {
+                for (int c = 0; c < input[r].Length; c++)
+                {
+                    maze[r, c] = input[r][c];
+
+                    if (maze[r, c] == '0')
+                    {
+                        startX = c;
+                        startY = r;
+                    }
+                    else if (char.IsDigit(maze[r, c]))
+                    {
+                        max = Math.Max(max, maze[r, c] - '0');
+                    }
+                }
+            }
+
+            State start = new State
+            {
+                x = startX,
+                y = startY,
+                visited = new bool[max],
+                maze = maze
+            };
+
+            State[] current = { start };
+            var seen = new HashSet<State>(current);
+
+            int steps = 0;
+            while (!current.Any(s => s.visited.All(v => v)))
+            {
+                var next = current.SelectMany(s => s.Next()).Distinct().ToArray();
+                var newNext = next.Where(s => !seen.Contains(s)).ToArray();
+
+                seen.UnionWith(newNext);
+
+                current = newNext;
+                steps++;
+            }
+
+            Console.WriteLine("Answer 1: {0}", steps);
+
+
+            current = new State[] { start };
+            seen = new HashSet<State>(current);
+
+            steps = 0;
+            while (!current.Any(s => s.x == startX && s.y == startY && s.visited.All(v => v)))
+            {
+                var next = current.SelectMany(s => s.Next()).Distinct().ToArray();
+                var newNext = next.Where(s => !seen.Contains(s)).ToArray();
+
+                seen.UnionWith(newNext);
+
+                current = newNext;
+                steps++;
+            }
+
+            Console.WriteLine("Answer 2: {0}", steps);
+
+            Console.ReadKey();
+        }
+    }
+}
